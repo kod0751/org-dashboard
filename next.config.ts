85275980fdee12725b1d3acac3335849ef488474
kod0751 <50727457+kod0751@users.noreply.github.com>
2025-11-10 23:58:49 +0900

    [chore] next.config image remotePattern 설정

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,18 @@ const nextConfig: NextConfig = {
   experimental: {
     reactCompiler: true,
   },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.pexels.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'search.pstatic.net',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
