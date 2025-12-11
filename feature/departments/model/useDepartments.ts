8174a03680853a03058e1b58d91cebd1a88f7954
kod0751 <50727457+kod0751@users.noreply.github.com>
2025-12-11 21:33:27 +0900

    [feat] React Query 훅 구현
    - useDepartments: 부서 목록 조회
    - useCreateDepartment: 부서 생성 및 캐시 갱신

---
+++ feature/departments/model/useDepartments.ts
@@ -0,0 +1,22 @@
+import { createDepartment } from '@/shared/api/departments/mutations';
+import { getDepartments } from '@/shared/api/departments/queries';
+import { Department } from '@/types/department';
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
+
+export function useDepartments() {
+  return useQuery<Department[]>({
+    queryKey: ['departments'],
+    queryFn: getDepartments,
+  });
+}
+
+export function useCreateDepartment() {
+  const queryClient = useQueryClient();
+
+  return useMutation({
+    mutationFn: createDepartment,
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ['departments'] });
+    },
+  });
+}
