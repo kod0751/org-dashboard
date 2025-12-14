2febc8593998c03daf3dd30b7453b403099ed2c2
kod0751 <50727457+kod0751@users.noreply.github.com>
2025-12-14 23:36:47 +0900

    [feat] soner Toaster 레이아웃 설정

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next';
 import { Geist, Geist_Mono } from 'next/font/google';
 import './globals.css';
 import QueryClientProviderWrapper from '@/shared/providers/react-query-provider';
+import { Toaster } from 'sonner';
 
 const geistSans = Geist({
   variable: '--font-geist-sans',
@@ -28,7 +29,10 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <QueryClientProviderWrapper>{children}</QueryClientProviderWrapper>
+        <QueryClientProviderWrapper>
+          {children}
+          <Toaster position="top-center" />
+        </QueryClientProviderWrapper>
       </body>
     </html>
   );
