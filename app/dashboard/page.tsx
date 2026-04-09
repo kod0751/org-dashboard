b63583062aaf22383c0f73956246778bc5260ebd
kod0751 <50727457+kod0751@users.noreply.github.com>
2026-04-10 01:32:05 +0900

    [chore]  주석 수정

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,24 +1,26 @@
-import StateCard from '@/components/dashboard/state-card';
-import RecentEmployees from '@/components/dashboard/recent-employees';
-import { DepartmentChart } from '../../components/charts/pie-chart';
-import { DashboardHeader } from '../../components/layout/header';
+import StateCard from "@/components/dashboard/state-card";
+import RecentEmployees from "@/components/dashboard/recent-employees";
+import { DepartmentChart } from "../../components/charts/pie-chart";
+import { DashboardHeader } from "../../components/layout/header";
 
 export default function DashboardPage() {
   return (
     <div className="bg-white min-h-full">
       <div className="flex flex-col gap-12 p-12">
-        {/* Header */}
+        {/* 헤더 */}
         <DashboardHeader title="Dashboard" />
 
-        {/* Stat Cards Component */}
+        {/* 메인 상태 카드 */}
         <div>
           <StateCard />
         </div>
 
+        {/* 메인 부서별 차트 */}
         <div>
           <DepartmentChart />
         </div>
 
+        {/* 최근 변경된 직원 */}
         <div>
           <RecentEmployees />
         </div>
